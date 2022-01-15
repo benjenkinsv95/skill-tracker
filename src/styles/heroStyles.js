8bd30082d07cc9461cfd3f6c14e3681cd56ad4f8
benjenkinsv95 <benjenkinsv95@gmail.com>
2022-01-15 15:23:37 -0600

    Steak margins and padding

--- src/styles/heroStyles.js
+++ src/styles/heroStyles.js
@@ -1,4 +1,4 @@
-const headerHeight = '64px'
+const headerHeight = '56px'
 const heroStyles = {
   display: 'flex',
   flexDirection: 'column',
@@ -8,7 +8,9 @@ const heroStyles = {
     'linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%)',
   color: 'white',
   /* Take up 30% of the viewport's height, (30 out of 100) */
-  height: `calc(100vh - ${headerHeight})`
+  height: `calc(100vh - ${headerHeight})`,
+  maxWidth: '100%',
+  margin: '0'
 }
 
 export default heroStyles
